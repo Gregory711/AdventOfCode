998adef31e35acd1c3609e8ee89242d98011cbc6
Gregory Travis <gregorytravis711@gmail.com>
2023-12-04 07:18:18 -0500

    Started day 4

---
+++ 2023/4/scratchcards.py
@@ -0,0 +1,12 @@
+"""
+scratchcards
+Elf scratched off a bunch of scratchcards and needs help seeing what he won
+Each card has two lists of numbers separated by |
+winning numbers then list of numbers you have
+first match makes card worth one point
+each subsequent match after first doubles the point value of the card!
+e.g.
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+worth 8 points
+Given a text file find the value of all the cards combined!
+"""
