d42367a70fad8c29ed20da3f5c914817cb2daf4f
Gregory Travis <gregorytravis711@gmail.com>
2023-12-04 07:41:28 -0500

    Finished scratchcards.py

--- 2023/4/scratchcards.py
+++ 2023/4/scratchcards.py
@@ -19,17 +19,21 @@ name = sys.argv[1]
 file1 = open(name, 'r')
 
 # Iterate over each card
+sum = 0
 for line in file1:
 	line = line.rstrip()
 	# Iterate over each winning number
+	win = set()
 	start = line.index(':') + 2
 	while start < len(line) and line[start] != '|':
 		while line[start] == ' ':
 			start += 1
 		end = line.index(' ', start)
+		win.add(int(line[start:end]))
 		#print(line[start:end])
 		start = end + 1
 	# Iterate over each selected number
+	points = 0
 	start = line.index(' ', start) +  1
 	while start < len(line):
 		while line[start] == ' ':
@@ -38,5 +42,10 @@ for line in file1:
 			end = line.index(' ', start)
 		except ValueError:
 			end = len(line)
-		print(line[start:end])
+		if int(line[start:end]) in win:
+			points = max(1, points * 2)
+		#print(line[start:end])
 		start = end + 1
+	#print(points)
+	sum += points
+print(sum)
