d1eef334aa4f5c791963a9a026aadb4051769800
Gregory Travis <gregorytravis711@gmail.com>
2023-12-05 21:16:08 -0500

    Updated algo desc to account for overlooked details

--- 2023/5/almanac2.py
+++ 2023/5/almanac2.py
@@ -1,19 +1,21 @@
 # Algorithm:
-# 1. Record each initial seed range as tuple in seeds list
-# 2. For each map do the following:
+# 1. Record each initial seed range as tuple in seeds list.
+# 2. For each range in each map do the following:
 #	a. For each seed range do the following:
 #		1. If a portion of the seed range is below the source range from the map then:
 #			Add a new range from bottom of seed range (inclusive) to bottom of the
-#			source range to a new seed list.
+#			source range to a temp seed list.
 #		2. If a portion of the seed range is on the source range from the map then:
 #			Add a new range from max(bottom of source range, bottom of the seed
-#			range) to min(top of source range, top of the seed range) to the new
+#			range) to min(top of source range, top of the seed range) to updated
 #			seed list (using adjusted numbers for range!).
 #		3. If a portion of the seed range is above the source range from the map then:
 #			Add a new range from top of source range (exclusive) to top of the
-#			seed range to a new seed list.
-#	b. Replace the old seed seed range with the new one
-# 3. Iterate over the final seed range and print the lowest lower bound
+#			seed range to a temp seed list.
+#	b. Replace the old seed seed range with the temp seed list.
+#	c. If that was the final map range then add the temp seed list to the update seed list
+#		and replace the seed range list with the updated list.
+# 3. Iterate over the final seed range and print the lowest lower bound.
 
 import sys
 
