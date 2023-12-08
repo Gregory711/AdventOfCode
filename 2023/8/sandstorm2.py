7b567f07d872e3c5be955ae9a67259b7d5a0bcd4
Gregory Travis <gregorytravis711@gmail.com>
2023-12-08 07:37:14 -0500

    Copied sandstorm to use in part 2

---
+++ 2023/8/sandstorm2.py
@@ -0,0 +1,45 @@
+import sys
+
+name = sys.argv[1]
+
+file1 = open(name, 'r')
+
+directions = ''
+hauntedMap = {}
+empty = True
+
+for line in file1:
+	line = line.rstrip()
+	if len(directions) == 0:
+		directions = line
+		continue
+	elif empty:
+		empty = False
+		continue
+	# Read in the map to get out of the haunted wasteland!
+	i = line.index(' ')
+	origin = line[0:i]
+	i = line.index('(') + 1
+	j = line.index(',')
+	left = line[i:j]
+	i = line.index(' ', i) + 1
+	j = line.index(')')
+	right = line[i:j]
+	hauntedMap[origin] = (left, right)
+#print(hauntedMap)
+
+found = False
+steps = 0
+loc = 'AAA'
+
+while not found:
+	for step in directions:
+		steps += 1
+		if step == 'L':
+			loc = hauntedMap[loc][0]
+		else:
+			loc = hauntedMap[loc][1]
+		if loc == 'ZZZ':
+			found = True
+			break
+print(steps)
