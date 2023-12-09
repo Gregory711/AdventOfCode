acc004a6433a195a8ab3bd09b3f5f80b7b0f92af
Gregory Travis <gregorytravis711@gmail.com>
2023-12-09 08:47:45 -0500

    Updated part 1 to read in the input data

--- 2023/9/oasis.py
+++ 2023/9/oasis.py
@@ -18,4 +18,25 @@ Add a prediction that is result of going up the tree starting with 0 then left +
 So 0 then 0 + 3 = 3 then 3 + 15 = 18
 Answer is just sum of the predictions (e.g. 18) for each (in test.txt its 114)
 Note: the real input contains negative numbers, is sometimes decreasing, and is sometimes not equal diffs
+
+Algorithm:
+Technically do not need to store the entire rows but input data small enough too and might need on part 2 so might as well.
+So start out by reading in row into list, will only store for duration of for in loop cycle to simplify things.
 '''
+
+import sys
+
+name = sys.argv[1]
+
+file1 = open(name, 'r')
+
+for line in file1:
+	values = []
+	i = 0
+	while i < len(line):
+		j = i + 1
+		while j < len(line) and line[j].isdigit():
+			j += 1
+		values.append(int(line[i:j]))
+		i = j + 1
+	print(values)
