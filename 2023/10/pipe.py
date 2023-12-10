6864a4eadb05561bfcf58a9ae778bf56392f2c4c
Gregory Travis <gregorytravis711@gmail.com>
2023-12-10 08:53:00 -0500

    Started part 1

---
+++ 2023/10/pipe.py
@@ -0,0 +1,21 @@
+# Algorithm:
+# Read in graph
+# Find S
+# Depth first search accounting for the pipe directions to get the path
+# Return half of the distance of the path rounding up e.g. in test data it would be ceil(7/2)=4
+
+import sys
+
+name = sys.argv[1]
+
+file1 = open(name, 'r')
+
+graph = []
+
+for line in file1:
+	line = line.rstrip()
+	row = []
+	for node in line:
+		row.append(node)
+	graph.append(row)
+print(graph)
