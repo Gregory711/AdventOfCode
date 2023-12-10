9679a4d2cd8b22ee4ef7092e9fbeb93efff285ad
Gregory Travis <gregorytravis711@gmail.com>
2023-12-10 09:24:52 -0500

    Continued work on part 1

--- 2023/10/pipe.py
+++ 2023/10/pipe.py
@@ -27,5 +27,29 @@ for r in range(len(graph)):
 		if graph[r][c] == 'S':
 			startRow = r
 			startColumn = c
-print(startRow)
-print(startColumn)
+#print(startRow)
+#print(startColumn)
+
+toVisit = []
+toVisit.append((startRow, startColumn))
+seen = set()
+found = False
+
+while not found:
+	visiting = toVisit.pop()
+	row = visiting[0]
+	col = visiting[1]
+	for r in range(-1, 1+1):
+		if r == -1 and row == 0:
+			continue
+		elif r == 1 and row == (len(graph)-1):
+			continue
+		for c in range(-1, 1+1):
+			if c == -1 and col == 0:
+				continue
+			elif c == 1 and col == (len(graph[r])-1):
+				continue
+			elif r == 0 and c == 0:
+				continue
+			print(graph[row+r][col+c])
+	found = True # temp test
