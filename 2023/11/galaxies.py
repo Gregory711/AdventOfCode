3e47894b702a6d160841b2d7de7baf33ae86efe3
Gregory Travis <gregorytravis711@gmail.com>
2023-12-11 07:28:55 -0500

    Started day 11 and added data

---
+++ 2023/11/galaxies.py
@@ -0,0 +1,11 @@
+'''
+First step will be to read in the galaxy coordinates as lists
+Double increment if row doesn't have any galaxies
+Once that is done make a list of all columns that don't have galaxies by iterating over coordinates
+Iterate over no galaxy column list and for each increment all galaxy coordinates with col greater and
+increment adjustment to use to calculate the next no galaxy column
+Iterate over each row and make a list containing number of galaxies in each row
+Iterate over each row again and use number list to calculate row part of solution
+Repeat process for column
+Print answer!
+'''
