7048e23847ac912044f5a55d6fa5965adb2389f9
Gregory Travis <gregorytravis711@gmail.com>
2023-12-14 07:45:45 -0500

    Started day 14 part 1

---
+++ 2023/14/rockAndRoll.py
@@ -0,0 +1,26 @@
+'''
+Start with platform like below:
+O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
+The 0s are rocks that can roll, the # are rocks that stay in place, and .s are empty.
+If you tilt the platform so all the 0s roll as far north as they can you end you end up with:
+OOOO.#.O.. 10
+OO..#....#  9
+OO..O##..O  8
+O..#.OO...  7
+........#.  6
+..#....#.#  5
+..O..#.O.O  4
+..O.......  3
+#....###..  2
+#....#....  1
+The numbers on right are their distance to south support beams, multiply it by them and you get 136 the total load and answer!
+'''
