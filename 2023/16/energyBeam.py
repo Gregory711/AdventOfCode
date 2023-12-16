fd81d4198d1b7257b74c52ae183d47c971fa74e4
Gregory Travis <gregorytravis711@gmail.com>
2023-12-16 08:29:23 -0500

    Planned helper classes and functions

--- 2023/16/energyBeam.py
+++ 2023/16/energyBeam.py
@@ -9,4 +9,21 @@ If it hits:
 - = splitter B such that it hits the flat side then it is split in 2 (left and rightward), hits pointy end goes out other pointy end
 Beams don't interact with other beams on tile (means we will have to record what direction beams are going on tile to avoid cycles)
 Tile is energized if tile has at least one beam pass through it, answer is number of energized tiles.
+
+Classes:
+-Cell
+ value: '.' or / or \ or | or -
+ left, right, up, down: boolean values are default to False, represent directions beams travel through this cell
+-Beam
+ row, col: int coordinates (0 indexed)
+ direction: char 'l' or r or u d for left/right/up/down respectively
+
+Functions:
+-getNewDirections
+ Params: Beam b, Cell[][] graph
+ Returns: char[] directions or None if hits wall
+-getNewRow
+ Params: int row, char direction
+ Returns: int newRow
+-getNewCol
 '''
