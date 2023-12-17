19c31e910359a59a5fe0501ce30e472e2b8cdf3d
Gregory Travis <gregorytravis711@gmail.com>
2023-12-17 12:18:15 -0500

    Wrote problem statement with example

---
+++ 2023/5/almanac2.py
@@ -0,0 +1,13 @@
+'''
+Example:
+-seeds: 79 14 55 13
+ Above describes 14 seeds from 79 to 92 (79+14-1) inclusive and also 55 to 67 so total of 27 seed numbers
+-seed-to-soil map:
+ 50 98  2
+ 52 50 48
+ Above contains two mappings of seed numbers to soil numbers
+ The first mapping maps seeds 98 to 99 (98+2-1) to 50 to 51
+ Any number that isn't in any of the maps is mapped to itself
+
+Problem is to map all seed numbers from the initial ranges all the way to location numbers and then print the smallest of those.
+'''
