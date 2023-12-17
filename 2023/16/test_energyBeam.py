5e63dece4d22861f5ddd7fdce998e32a8a7c8870
Gregory Travis <gregorytravis711@gmail.com>
2023-12-16 21:56:09 -0500

    Added more tests

--- 2023/16/test_energyBeam.py
+++ 2023/16/test_energyBeam.py
@@ -3,3 +3,7 @@ import energyBeam as EB
 def test_getNewDirections():
 	directions = EB.getNewDirections(EB.Direction.LEFT, '.')
 	assert directions == [EB.Direction.LEFT]
+	directions = EB.getNewDirections(EB.Direction.UP, '|')
+	assert directions == [EB.Direction.UP]
+	directions = EB.getNewDirections(EB.Direction.LEFT, '-')
+	assert directions == [EB.Direction.LEFT]
