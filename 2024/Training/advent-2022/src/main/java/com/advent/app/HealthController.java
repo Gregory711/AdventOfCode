8b54193f0b472bc68f1bcb0b494f7e05950a3fed
Gregory Travis <gregorytravis711@gmail.com>
2024-02-23 22:24:04 -0500

    Updated health endpoint to return message and status code

--- 2024/Training/advent-2022/src/main/java/com/advent/app/HealthController.java
+++ 2024/Training/advent-2022/src/main/java/com/advent/app/HealthController.java
@@ -1,5 +1,10 @@
 package com.advent.app;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping("/health")
-    public String health() {
-        return "OK";
+    public ResponseEntity<Map<String, Object>> health() {
+        Map<String, Object> data = new HashMap<>();
+        data.put("message", "I'm alive!");
+        return ResponseEntity.status(HttpStatus.OK).body(data);
     }
 }
-
