19c5d2256e18a05c1144de277cb2e5aa93277123
Gregory Travis <gregorytravis711@gmail.com>
2024-04-25 21:52:55 -0400

    Updated generic day endpoint and added comment explaining new approach

--- 2024/Training/advent-2022/src/main/java/com/advent/app/DayController.java
+++ 2024/Training/advent-2022/src/main/java/com/advent/app/DayController.java
@@ -13,13 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class DayController {
     
     @GetMapping("/day")
-    public ResponseEntity<Map<String, Object>> day(@RequestParam String day, @RequestParam String file) {
+    public ResponseEntity<Map<String, Object>> day(@RequestParam int day) {
         Map<String, Object> data = new HashMap<>();
-        data.put("answer", calculateAnswer(day, file));
+        data.put("answer", generateReport(day));
         return ResponseEntity.status(HttpStatus.OK).body(data);
     }
 
-    private String calculateAnswer(String day, String file) {
+    /*
+     * Generates a report for the given day of the month containing:
+     * For each ${day}test${numb}.txt file in the resources folder:
+     *   - The calculated answer and if it matches the expected answer
+     *   - The time taken to calculate the answer
+     * For the ${day}input.txt file in the resources folder:
+     *   - The calculated answer
+     *   - The time taken to calculate the answer
+     * @param day The day of the month to generate a report for
+     * @return The report for the given day
+     */
+    private String generateReport(int day) {
         return "ToDo: Implement me!";
     }
 }
