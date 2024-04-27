c1e0ecea185882abdf654bb0f1f1eb564791a1b4
Gregory Travis <gregorytravis711@gmail.com>
2024-04-27 11:38:49 -0400

    Start CalorieCounter class

---
+++ 2024/Training/advent-2022/src/main/java/com/advent/app/CalorieCounter.java
@@ -0,0 +1,12 @@
+package com.advent.app;
+
+import java.util.ArrayList;
+
+public class CalorieCounter {
+
+    private ArrayList<String> input;
+
+    public CalorieCounter(ArrayList<String> input) {
+        this.input = input;
+    }
+}
