7dd348bd9f661e133b0c0855f66f5e2377fe82a9
Gregory Travis <gregorytravis711@gmail.com>
2024-04-27 11:43:23 -0400

    Added getMostCalories function

--- 2024/Training/advent-2022/src/main/java/com/advent/app/CalorieCounter.java
+++ 2024/Training/advent-2022/src/main/java/com/advent/app/CalorieCounter.java
@@ -9,4 +9,17 @@ public class CalorieCounter {
     public CalorieCounter(ArrayList<String> input) {
         this.input = input;
     }
+
+    public int getMostCalories() {
+        int max = 0, elf = 0;
+        for (String line : input) {
+            if (line.length() > 0) {
+                elf += Integer.parseInt(line);
+            } else {
+                elf = 0;
+            }
+            max = Math.max(max, elf);
+        }
+        return max;
+    }
 }
