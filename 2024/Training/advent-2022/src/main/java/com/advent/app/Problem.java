089650fde4d62a4b7dbf17dfb00e13315a6c86d0
Gregory Travis <gregorytravis711@gmail.com>
2024-04-27 12:40:51 -0400

    Added expected answers to Problem class

--- 2024/Training/advent-2022/src/main/java/com/advent/app/Problem.java
+++ 2024/Training/advent-2022/src/main/java/com/advent/app/Problem.java
@@ -1,17 +1,19 @@
 package com.advent.app;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Problem {
 
     private final int day;
-    private final boolean isTest;
     private ArrayList<String> input;
+    // null if not a test
+    private final String expectedAnswer;
 
-    public Problem(int day, boolean isTest, ArrayList<String> input) {
+    public Problem(int day, ArrayList<String> input, String expectedAnswer) {
         this.day = day;
-        this.isTest = isTest;
         this.input = input;
+        this.expectedAnswer = expectedAnswer;
     }
     
 }
