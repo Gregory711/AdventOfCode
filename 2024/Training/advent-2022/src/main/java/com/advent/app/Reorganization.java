d59590420e8d40d8d571b9ee54e42e3bcce2a620
Gregory Travis <gregorytravis711@gmail.com>
2024-05-10 21:39:14 -0400

    Started Rucksack class

--- 2024/Training/advent-2022/src/main/java/com/advent/app/Reorganization.java
+++ 2024/Training/advent-2022/src/main/java/com/advent/app/Reorganization.java
@@ -1,8 +1,20 @@
 package com.advent.app;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Reorganization {
+
+    private class Rucksack {
+        private String compartments[];
+
+        public Rucksack(String compartments) {
+            this.compartments = new String[2];
+            int midpoint = compartments.length() / 2;
+            this.compartments[0] = compartments.substring(0, midpoint);
+            this.compartments[1] = compartments.substring(midpoint, compartments.length());
+        }
+    }
 
     public Reorganization(ArrayList<String> input) {
 
