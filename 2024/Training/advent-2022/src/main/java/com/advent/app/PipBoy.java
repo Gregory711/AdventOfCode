21c2ff03cb7c8e6eea3c9518b162aa5dc6d45068
Gregory Travis <gregorytravis711@gmail.com>
2024-05-25 12:49:27 -0400

    Setup structure

--- 2024/Training/advent-2022/src/main/java/com/advent/app/PipBoy.java
+++ 2024/Training/advent-2022/src/main/java/com/advent/app/PipBoy.java
@@ -3,8 +3,16 @@ package com.advent.app;
 import java.util.ArrayList;
 
 public class PipBoy {
+
+    int indexEndOfFirstMarker;
     
     public PipBoy(ArrayList<String> input) {
-        //
+        String buffer = input.get(0);
+
+        for (int i = 3; i < buffer.length(); i++) {
+            // If this is the end of the first marker then set the
+            // corresponding index and break out of the loop
+        }
     }
+
 }
