6fee9bb983cb367a9f1164d743e1f380e47641a5
Gregory Travis <gregorytravis711@gmail.com>
2024-06-01 14:09:53 -0400

    Getting setup to parse cmds

--- 2024/Training/advent-2022/src/main/java/com/advent/app/Filesystem.java
+++ 2024/Training/advent-2022/src/main/java/com/advent/app/Filesystem.java
@@ -5,6 +5,15 @@ import java.util.ArrayList;
 public class Filesystem {
     
     public Filesystem(ArrayList<String> input) {
-        //
+
+        String cmd, currDir;
+        boolean inOutput = false;
+
+        for (int i = 0; i < input.size(); i++) {
+            // If not still listing output from ls cmd then intrepret the current cmd
+            if (!inOutput) {
+                cmd = input.get(i);
+            }
+        }
     }
 }
