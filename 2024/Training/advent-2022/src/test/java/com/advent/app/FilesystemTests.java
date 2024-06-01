f16c95e3e05bcc805c8bedd1f913165b60931ba2
Gregory Travis <gregorytravis711@gmail.com>
2024-06-01 16:25:01 -0400

    Fixed test data

--- 2024/Training/advent-2022/src/test/java/com/advent/app/FilesystemTests.java
+++ 2024/Training/advent-2022/src/test/java/com/advent/app/FilesystemTests.java
@@ -11,21 +11,21 @@ public class FilesystemTests {
     @Test
     public void shouldFunction() {
         ArrayList<String> input = new ArrayList<String>();
-        input.add("cd /");
-        input.add("ls");
+        input.add("$ cd /");
+        input.add("$ ls");
         input.add("dir a");
         input.add("123 a.txt");
-        input.add("cd a");
-        input.add("ls");
+        input.add("$ cd a");
+        input.add("$ ls");
         input.add("dir b");
         input.add("dir c");
         input.add("345 b.txt");
-        input.add("cd b");
-        input.add("ls");
+        input.add("$ cd b");
+        input.add("$ ls");
         input.add("678 c.txt");
-        input.add("cd ..");
-        input.add("cd c");
-        input.add("ls");
+        input.add("$ cd ..");
+        input.add("$ cd c");
+        input.add("$ ls");
         input.add("dir d");
 
         Filesystem fs = new Filesystem(input);
