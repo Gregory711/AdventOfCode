8da8695939fda3ac7a7cce9c60e62ec1f156ff06
Gregory Travis <gregorytravis711@gmail.com>
2024-06-01 14:16:48 -0400

    Switched currDirectory to Stack to support backing out and setup if structure for cd parsing

--- 2024/Training/advent-2022/src/main/java/com/advent/app/Filesystem.java
+++ 2024/Training/advent-2022/src/main/java/com/advent/app/Filesystem.java
@@ -1,18 +1,31 @@
 package com.advent.app;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Filesystem {
     
     public Filesystem(ArrayList<String> input) {
 
-        String cmd, currDir;
+        Stack<String> currDir = new Stack<String>();
+        String cmd;
         boolean inOutput = false;
 
         for (int i = 0; i < input.size(); i++) {
             // If not still listing output from ls cmd then intrepret the current cmd
             if (!inOutput) {
                 cmd = input.get(i);
+                // Parse if cmd is change directory or list
+                if (cmd.contains("cd")) {
+                    // Parse if cmd is switching to root /, backing out .. or moving in
+                    if (cmd.contains("/")) {
+                        //
+                    } else if (cmd.contains("..")) {
+                        //
+                    } else {
+                        //
+                    }
+                }
             }
         }
     }
