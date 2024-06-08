b08bb5fe8d99fd416ac7cf96f088e8538e7e8a52
Gregory Travis <gregorytravis711@gmail.com>
2024-06-08 13:33:48 -0400

    Added another test filesystem with non unique directory names and added corresponding unit test

--- 2024/Training/advent-2022/src/test/java/com/advent/app/FilesystemTests.java
+++ 2024/Training/advent-2022/src/test/java/com/advent/app/FilesystemTests.java
@@ -14,6 +14,7 @@ import com.advent.app.Filesystem.Directory;
 public class FilesystemTests {
 
     private static Filesystem fs;
+    private static Filesystem fs2;
 
     @BeforeClass
     public static void init() {
@@ -48,6 +49,26 @@ public class FilesystemTests {
          * 
          * So / has size 123+345+678=1146, a=678+345=1023, b=678, c=d=0
          */
+
+         ArrayList<String> input2 = new ArrayList<String>();
+         input2.add("$ cd /");
+         input2.add("$ ls");
+         input2.add("dir a");
+         input2.add("dir b");
+         input2.add("$ cd a");
+         input2.add("$ ls");
+         input2.add("dir a");
+         input2.add("dir b");
+         fs2 = new Filesystem(input2);
+         /**
+          * /
+              a
+                a
+                b
+              b
+
+          So five directories: /, /a, /a/a, /a/b/, /b
+          */
     }
 
     /**
@@ -74,5 +95,16 @@ public class FilesystemTests {
         assertEquals(678, fs.getDirectorySize("b"));
         assertEquals(0, fs.getDirectorySize("c"));
         assertEquals(0, fs.getDirectorySize("d"));
+    }
+
+    /*
+     * Verifies fs2 has the correct directories
+     */
+    @Test
+    public void hasCorrectDirectories2() {
+        HashMap<String, Directory> dirs = fs2.getDirectories();
+
+        assertEquals(5*2, dirs.size());
+        // TODO: assert individual directories included
     }
 }
