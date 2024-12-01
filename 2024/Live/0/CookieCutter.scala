285ba1857eb18071fc133b7c80fb08bd703b65db
Gregory Travis <gregorytravis711@gmail.com>
2024-12-01 00:01:29 -0500

    Read in text file in template

--- 2024/Live/0/CookieCutter.scala
+++ 2024/Live/0/CookieCutter.scala
@@ -1,5 +1,11 @@
+import scala.io.Source
+
 object Main {
   def main(args: Array[String]): Unit = {
     println("Advent of Code!")
+
+    val filename = "cookies.txt"
+    val lines = Source.fromFile(filename).getLines().toList
+    lines.foreach(println)
   }
 }
