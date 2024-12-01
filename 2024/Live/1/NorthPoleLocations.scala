f61689a8c5d25ce339751df846e5e60e31a533a4
Gregory Travis <gregorytravis711@gmail.com>
2024-12-01 11:40:39 -0500

    Converted to int earlier

--- 2024/Live/1/NorthPoleLocations.scala
+++ 2024/Live/1/NorthPoleLocations.scala
@@ -6,16 +6,16 @@ object Main {
   def main(args: Array[String]): Unit = {
     val filename = "lists.txt"
     val lines = Source.fromFile(filename).getLines().toList
-    val listA = ArrayBuffer[String]()
-    val listB = ArrayBuffer[String]()
+    val listA = ArrayBuffer[Int]()
+    val listB = ArrayBuffer[Int]()
     val occurrences = HashMap[Int, Int]()
     lines.foreach(line =>
         // Split breaks String into String Array
         // " +" is regex for one or more of what came before it which in this case is a space
         // So splits line into an array of strings broken up by spaces
         val ids = line.split(" +");
-        listA.append(ids(0))
-        listB.append(ids(1))
+        listA.append(ids(0).toInt)
+        listB.append(ids(1).toInt)
         //println(ids(0) + ", " + ids(1));
     )
     val listASorted = listA.sorted
@@ -25,7 +25,7 @@ object Main {
         i <- 0 to listASorted.size - 1
     do
         //println(listASorted(i) + ", " + listBSorted(i))
-        distance += (listASorted(i).toInt - listBSorted(i).toInt).abs
+        distance += (listASorted(i) - listBSorted(i)).abs
         //println("Distance: " + distance)
     println("The total distance is " + distance)
   }
