64c4828f02f7bf146cdb275e23c596563e05f9f6
Gregory Travis <gregorytravis711@gmail.com>
2024-12-01 00:20:06 -0500

    Processing input

--- 2024/Live/1/NorthPoleLocations.scala
+++ 2024/Live/1/NorthPoleLocations.scala
@@ -5,7 +5,11 @@ object Main {
     val filename = "lists.txt"
     val lines = Source.fromFile(filename).getLines().toList
     lines.foreach(line =>
-        println(line);
+        // Split breaks String into String Array
+        // " +" is regex for one or more of what came before it which in this case is a space
+        // So splits line into an array of strings broken up by spaces
+        val ids = line.split(" +");
+        println(ids(0) + ", " + ids(1));
     )
   }
 }
