6d65bd4860c5e36ed1fe2bf92eb0d4bf418869a3
Gregory Travis <gregorytravis711@gmail.com>
2024-12-02 08:09:18 -0500

    Refactored to use function so can start part 2

--- 2024/Live/2/nuclear.scala
+++ 2024/Live/2/nuclear.scala
@@ -1,6 +1,34 @@
 import scala.io.Source
 
 object Main {
+    def reportIsSafe(levels: Array[String], dampen: Int): Boolean = {
+        var first: Boolean = true
+        var increasing: Boolean = false
+        var decreasing: Boolean = false
+        var gradual: Boolean = true
+        var last: Int = -1
+        levels.foreach(level =>
+            //println("Processing level: " + level)
+            if (!first) {
+                if (level.toInt > last) {
+                    increasing = true
+                } else if (level.toInt < last) {
+                    decreasing = true
+                }
+                val diff: Int = (level.toInt - last).abs
+                if (diff < 1 || diff > 3) {
+                    gradual = false
+                }
+            }
+            last = level.toInt
+            first = false
+        )
+        if (gradual && (increasing != decreasing)) {
+            return true
+        }
+        return false
+    }
+
     def main(args: Array[String]): Unit = {
         val filename = "reports.txt"
         val lines = Source.fromFile(filename).getLines().toList
@@ -8,28 +36,7 @@ object Main {
 
         lines.foreach(line =>
             val levels = line.split(" +")
-            var first: Boolean = true
-            var increasing: Boolean = false
-            var decreasing: Boolean = false
-            var gradual: Boolean = true
-            var last: Int = -1
-            levels.foreach(level =>
-                //println("Processing level: " + level)
-                if (!first) {
-                    if (level.toInt > last) {
-                        increasing = true
-                    } else if (level.toInt < last) {
-                        decreasing = true
-                    }
-                    val diff: Int = (level.toInt - last).abs
-                    if (diff < 1 || diff > 3) {
-                        gradual = false
-                    }
-                }
-                last = level.toInt
-                first = false
-            )
-            if (gradual && (increasing != decreasing)) {
+            if (reportIsSafe(levels, -1)) {
                 safeReportCount = safeReportCount + 1
             }
         )
