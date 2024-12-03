b5f7c0ef7dc624b7f429e333466aeb593578e9eb
Gregory Travis <gregorytravis711@gmail.com>
2024-12-03 08:14:04 -0500

    Fixed bug to finish day 3 part 1

--- 2024/Live/3/computer.scala
+++ 2024/Live/3/computer.scala
@@ -3,28 +3,33 @@ import scala.io.Source
 object Main {
     // Returns (value of next mul after index or 0 if none or invalid, index incremented past end of mul or end of string if none)
     def getMulValue(instructions: String, startIndex: Int): (Int, Int) = {
+        //println("Entering getMulValue with startIndex: " + startIndex)
         val mulIndex = instructions.indexOf("mul(", startIndex)
+        //println("  mulIndex is " + mulIndex)
         // If no mul instruction found return end of string
         if (mulIndex == -1) {
             return (0, instructions.size)
         }
         // Find comma ending first mul arg, return end of string if none found
         val commaIndex = instructions.indexOf(",", mulIndex)
+        //println("  commaIndex is " + commaIndex)
         if (commaIndex == -1) {
             return (0, instructions.size)
         }
         // Find closing parentheses ending mul call, return end of string if none found
         val closeIndex = instructions.indexOf(")", commaIndex)
+        //println("  closeIndex is " + closeIndex)
         if (closeIndex == -1) {
             return (0, instructions.size)
         }
         // Try to parse first mul arg, if failed return end of mul(
-        val argAOption = instructions.slice(mulIndex, commaIndex).toIntOption
+        val argAOption = instructions.slice(mulIndex + "mul(".size, commaIndex).toIntOption
         val argA = argAOption match {
             case Some(arg) => arg
             case None =>
                 return (0, mulIndex + 1)
         }
+        //println("  Arg A is " + argA)
         // Try to parse second mul arg, if failed return end of ,
         val argBOption = instructions.slice(commaIndex + 1, closeIndex).toIntOption
         val argB = argBOption match {
@@ -32,6 +37,7 @@ object Main {
             case None =>
                 return (0, commaIndex + 1)
         }
+        //println("  Arg B is " + argB)
         // Return result of calculating mul instruction!
         return (argA * argB, closeIndex + 1)
     }
