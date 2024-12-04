58c5e9a43fb604239df67278000258bca5904781
Gregory Travis <gregorytravis711@gmail.com>
2024-12-04 08:03:28 -0500

    Added deltas to navigate word search with

--- 2024/Live/4/xmas.scala
+++ 2024/Live/4/xmas.scala
@@ -5,6 +5,18 @@ object Main {
         val filename = "wordSearch.txt"
         val lines = Source.fromFile(filename).getLines().toList
 
+        case class Coordinate(x: Int, y: Int)
+        val deltas: List[Coordinate] = List(
+            Coordinate(-1, -1), // diagonal up left
+            Coordinate(0, -1), // up
+            Coordinate(1, -1), // diagonal up right
+            Coordinate(1, 0), // right
+            Coordinate(1, 1), // diagonal down right
+            Coordinate(0, 1), // down
+            Coordinate(-1, 1), // diagonal down left
+            Coordinate(-1, 0) // left
+        )
+
         for
             i <- 0 to lines.size - 1
         do
