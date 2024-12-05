61088bbd014e5e96309765bdd9a13e5aca30e457
Gregory Travis <gregorytravis711@gmail.com>
2024-12-05 07:56:22 -0500

    Throw values that come before in hashmap

--- 2024/Live/5/manuals.scala
+++ 2024/Live/5/manuals.scala
@@ -1,16 +1,22 @@
 import scala.io.Source
+import scala.collection.mutable.HashMap
 
 object Main {
     def main(args: Array[String]): Unit = {
         val filename = "pages.txt"
         val lines = Source.fromFile(filename).getLines().toList
 
+        var before = new HashMap[Int, List[Int]]()
         lines.foreach(line =>
             val pipe: Int = line.indexOf('|')
             if (pipe != -1) {
                 val a: Int = line.slice(0, pipe).toInt
                 val b: Int = line.substring(pipe + 1).toInt
                 //println(a + " must come before "+ b)
+                if (!before.contains(b)) {
+                    before(b) = List.empty[Int]
+                }
+                before(b).prepended(a)
             }
         )
     }
