f3476f771ccd2cd41680225f367575797ced691a
Gregory Travis <gregorytravis711@gmail.com>
2024-12-04 22:31:03 -0500

    Finished part 2 of day 4

--- 2024/Live/4/xmas.scala
+++ 2024/Live/4/xmas.scala
@@ -2,7 +2,7 @@ import scala.io.Source
 
 object Main {
     def main(args: Array[String]): Unit = {
-        val filename = "wordSearch.txt"
+        val filename = "input.txt"
         val lines = Source.fromFile(filename).getLines().toList
 
         case class Coordinate(x: Int, y: Int)
@@ -17,11 +17,24 @@ object Main {
             Coordinate(-1, 0) // left
         )
 
+        val diagonalPairs: List[List[Coordinate]] = List(
+            List(
+                Coordinate(-1, -1), // diagonal up left
+                Coordinate(1, 1) // diagonal down right
+            ),
+            List(
+                Coordinate(-1, 1), // diagonal down left
+                Coordinate(1, -1) // diagonal up right
+            )
+        )
+
         var xmasCount: Int = 0
+        var masCount: Int = 0
 
         for
             i <- 0 to lines.size - 1
         do
+            // Part 1
             for (j <- 0 to lines(i).size - 1 if lines(i)(j) == 'X') {
                 deltas.foreach(delta =>
                     val M: Coordinate = Coordinate(i + delta.x, j + delta.y)
@@ -37,6 +50,33 @@ object Main {
                     }
                 )
             }
+            // Part 2
+            for (j <- 0 to lines(i).size - 1 if lines(i)(j) == 'A') {
+                var masDiagonals: Boolean = true
+                diagonalPairs.foreach(diagonal => 
+                    val left: Coordinate = Coordinate(i + diagonal(0).x, j + diagonal(0).y)
+                    val right: Coordinate = Coordinate(i + diagonal(1).x, j + diagonal(1).y)
+                    // Check if left and right are in bounds
+                    if (
+                        left.x >= 0 && left.y >= 0 && left.x < lines.size && left.y < lines(left.x).size &&
+                        right.x >= 0 && right.y >= 0 && right.x < lines.size && right.y < lines(left.y).size
+                    ) {
+                        // Left and right must use S and M between them or not an X-MAS formation
+                        if (
+                            !(lines(left.x)(left.y) == 'M' && lines(right.x)(right.y) == 'S') &&
+                            !(lines(left.x)(left.y) == 'S' && lines(right.x)(right.y) == 'M') 
+                        ){
+                            masDiagonals = false
+                        }
+                    } else {
+                        masDiagonals = false
+                    }
+                )
+                if (masDiagonals) {
+                    masCount = masCount + 1
+                }
+            }
         println("XMAS appears in the word search " + xmasCount + " times!")
+        println("X-MAS formation appears in the word search " + masCount + " times!")
     }
 }
