b0851636a143f0bddf44c3c9a1cb471a656086bd
Gregory Travis <gregorytravis711@gmail.com>
2024-12-05 08:14:01 -0500

    First attempt at day 5 part 1

--- 2024/Live/5/manuals.scala
+++ 2024/Live/5/manuals.scala
@@ -1,5 +1,6 @@
 import scala.io.Source
 import scala.collection.mutable.HashMap
+import scala.collection.mutable.HashSet
 
 object Main {
     def main(args: Array[String]): Unit = {
@@ -9,6 +10,7 @@ object Main {
         var before = new HashMap[Int, List[Int]]()
         lines.foreach(line =>
             val pipe: Int = line.indexOf('|')
+            val comma: Int = line.indexOf(',')
             if (pipe != -1) {
                 val a: Int = line.slice(0, pipe).toInt
                 val b: Int = line.substring(pipe + 1).toInt
@@ -17,6 +19,25 @@ object Main {
                     before(b) = List.empty[Int]
                 }
                 before(b).prepended(a)
+            } else if (comma != -1) {
+                val pages = line.split(",+")
+                var addMiddle: Boolean = true
+                //println(pages.toList)
+                var rulesBarred = HashSet[Int]()
+                pages.foreach(page =>
+                    if (rulesBarred.contains(page.toInt)) {
+                        addMiddle = false
+                    } else if (addMiddle) {
+                        if (before.contains(page.toInt)) {
+                            before(page.toInt).foreach(rule =>
+                                rulesBarred.add(rule)
+                            )
+                        }
+                    }
+                )
+                if (addMiddle) {
+                    println("Should add middle number for this report: " + line)
+                }
             }
         )
     }
