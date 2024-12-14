157dbc4a8132d39d4e9afc1cb65766aa42594b91
Gregory Travis <gregorytravis711@gmail.com>
2024-12-14 12:57:07 -0500

    Added skeleton of recursive solver function

--- 2024/Live/7/jungleMath.scala
+++ 2024/Live/7/jungleMath.scala
@@ -1,15 +1,19 @@
 import scala.io.Source
 
 object Main {
+    def solve(numbs: Array[Int], operators: Array[Char], result: Int, index: Int): Boolean = {
+        return false
+    }
+
     def main(args: Array[String]): Unit = {
         val filename = "bridge.txt"
         val lines = Source.fromFile(filename).getLines().toList
 
         lines.foreach(line =>
             val colonIndex: Int = line.indexOf(':')
-            val sum: Int = line.slice(0, colonIndex).toInt
+            val result: Int = line.slice(0, colonIndex).toInt
             val numbs: Array[Int] = line.substring(colonIndex+2).split(" +").map(_.toInt)
-            println("Test sum: " + sum)
+            println("Test result: " + result)
             println("Numbers: " + numbs.mkString(", "))
             println()
         )
