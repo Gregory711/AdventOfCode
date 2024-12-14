eef658183714a433b93ac1b5daf3b059f2329e8f
Gregory Travis <gregorytravis711@gmail.com>
2024-12-14 17:03:08 -0500

    Found and stored antenna locations by frequency

--- 2024/Live/8/antennas.scala
+++ 2024/Live/8/antennas.scala
@@ -1,7 +1,12 @@
 import scala.io.Source
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
 
 object Main {
+    // Just to clarify each row of roof has different x starting with 0 (top/first line)
+    // Each column has y starting with 0 leftmost
+    case class Coordinate(x: Int, y: Int)
+
     def printRoof(roof: ArrayBuffer[Array[Char]]): Unit = {
         println("Current state of roof:")
         roof.foreach(line =>
@@ -20,5 +25,21 @@ object Main {
         )
 
         printRoof(roof)
+
+        // Get locations of antennas
+        // Maps frequencies to antenna locations
+        val antennas = new HashMap[Char, List[Coordinate]]()
+        for (x <- roof.indices) {
+            val row: Array[Char] = roof(x)
+            for (y <- row.indices) {
+                val antenna: Char = row(y)
+                if (antenna != '.') {
+                    if (!antennas.contains(antenna)) {
+                        antennas(antenna) = List.empty[Coordinate]
+                    }
+                    antennas(antenna) = antennas(antenna).prepended(Coordinate(x, y))
+                }
+            }
+        }
     }
 }
