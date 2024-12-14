4ea89843b3fbee43a755d1e8d5c34852d8976fab
Gregory Travis <gregorytravis711@gmail.com>
2024-12-14 13:07:57 -0500

    Setup main function to call recursive solver and output result

--- 2024/Live/7/jungleMath.scala
+++ 2024/Live/7/jungleMath.scala
@@ -9,13 +9,21 @@ object Main {
         val filename = "bridge.txt"
         val lines = Source.fromFile(filename).getLines().toList
 
+        var resultSum: Int = 0
         lines.foreach(line =>
             val colonIndex: Int = line.indexOf(':')
             val result: Int = line.slice(0, colonIndex).toInt
             val numbs: Array[Int] = line.substring(colonIndex+2).split(" +").map(_.toInt)
+            val operators: Array[Char] = new Array[Char](numbs.size - 1)
+
             println("Test result: " + result)
             println("Numbers: " + numbs.mkString(", "))
+
+            if (solve(numbs, operators, result, 0)) {
+                resultSum = resultSum + result
+            }
             println()
         )
+        println("The sum of results of valid bridge equations is " + resultSum)
     }
 }
