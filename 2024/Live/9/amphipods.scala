524c7f3604d7db641a0837237b483dd3b5d90ded
Gregory Travis <gregorytravis711@gmail.com>
2024-12-15 12:48:17 -0500

    Calculated free space and added mini test file

--- 2024/Live/9/amphipods.scala
+++ 2024/Live/9/amphipods.scala
@@ -8,6 +8,12 @@ object Main {
         // ASCII addition e.g. '0' - '0' is 0 int, '1' - '0' is 1 int, and so on
         val disk: Array[Int] = lines.head.toCharArray().map(_ - '0')
 
+        var freeSpace: Int = 0
+        for (i <- 0 until disk.size if i % 2 != 0) {
+            freeSpace = freeSpace + disk(i)
+        }
+
         println("Initial Disk: " + disk.mkString)
+        println("Free space = " + freeSpace)
     }
 }
