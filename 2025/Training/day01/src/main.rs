32c2e0b2ece6da28057e66e0627e383e4034127d
Gregory Travis <gregorytravis711@gmail.com>
2025-11-30 14:44:42 -0500

    Got file io ready to go

--- 2025/Training/day01/src/main.rs
+++ 2025/Training/day01/src/main.rs
@@ -1,3 +1,14 @@
+fn part1(input: &String) {
+    for line in input.lines() {
+        println!("line = {}", line);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input.txt")
+        .expect("Failed to read input file")
+        .trim_end()
+        .to_string();
+
+    part1(&input)
 }
