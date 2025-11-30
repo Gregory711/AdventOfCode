a19e946e783e487a8c7c1fb5e40da9bde609ec85
Gregory Travis <gregorytravis711@gmail.com>
2025-11-30 15:57:28 -0500

    Completed part 1!

--- 2025/Training/day01/src/main.rs
+++ 2025/Training/day01/src/main.rs
@@ -1,14 +1,28 @@
+// advent-2021 day 1
+
 fn part1(input: &String) {
+    let mut increases: i32 = 0;
+    let mut last_number: Option<i64> = None;
+
     for line in input.lines() {
-        println!("line = {}", line);
+        let current_number: i64 = line.parse().unwrap();
+        if last_number.is_some() && current_number > last_number.unwrap() {
+            increases += 1;
+        }
+        last_number = Some(current_number);
     }
+
+    println!("There were {} increases!", increases);
 }
 
 fn main() {
-    let input = std::fs::read_to_string("input.txt")
-        .expect("Failed to read input file")
-        .trim_end()
-        .to_string();
+    for &file in &["test.txt", "input.txt"] {
+        let input = std::fs::read_to_string(format!("day01/{}", file))
+            .expect(&format!("Failed to read file: {}", file))
+            .trim_end()
+            .to_string();
 
-    part1(&input)
+        println!("Part 1: {}:", file);
+        part1(&input);
+    }
 }
