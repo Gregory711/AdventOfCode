36f243947a228f5df3315c8717952ee1d438c2cd
Gregory Travis <gtravis6@gatech.edu>
2025-12-01 08:15:31 -0500

    Finished day 1

--- 2025/Live/day01/src/main.rs
+++ 2025/Live/day01/src/main.rs
@@ -1,16 +1,21 @@
 // advent-2025 day 1
 
-fn part1(input: &String) {
+fn print_password(input: &String, part: i32) {
     let mut password: i64 = 0;
     let mut dial: i64 = 50;
 
     for line in input.lines() {
         let rotations: String = line.chars().skip(1).collect();
         let mut rotations: i64 = rotations.parse().unwrap();
+        let dial_started_at_zero = dial == 0;
 
         // deal with full cycles
         if rotations >= 100 {
-            // set rotations to remainder after full cycles are taken into acccount
+            // set rotations to remainder after full cycles are taken into account
+            // if part 1 then we don't add full cycles to password, on part 2 we do
+            if part == 2 {
+                password += rotations / 100;
+            }
             rotations %= 100;
         }
 
@@ -22,16 +27,28 @@ fn part1(input: &String) {
         }
 
         // account for partial cycles
+        // if on part 2 we add partial cycles to password (avoiding double counting)
+        // only counts if clicked to zero not if started on zero, account for that
+        if part == 2 && dial_started_at_zero && (dial < 0 || dial > 99) {
+            password -= 1;
+        }
         if dial < 0 {
             dial += 100;
+            if part == 2 && dial != 0 {
+                password += 1;
+            }
         } else if dial > 99 {
             dial -= 100;
+            if part == 2 && dial != 0 {
+                password += 1;
+            }
         }
 
         // increment password
         if dial == 0 {
             password += 1;
         }
+        //println!("The line is {} and the password is {}", line, password);
     }
     println!("The password is: {}", password);
 }
@@ -44,7 +61,8 @@ fn main() {
             .to_string();
 
         println!("Part 1: {}:", file);
-        part1(&input);
+        print_password(&input, 1);
         println!("Part 2: {}:", file);
+        print_password(&input, 2);
     }
 }
