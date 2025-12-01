3d1bcd57f1d941fdcc3ef824371aee1eb6722b4a
Gregory Travis <gtravis6@gatech.edu>
2025-12-01 07:58:26 -0500

    Completed part 1

--- 2025/Live/day01/src/main.rs
+++ 2025/Live/day01/src/main.rs
@@ -1,9 +1,39 @@
 // advent-2025 day 1
 
 fn part1(input: &String) {
+    let mut password: i64 = 0;
+    let mut dial: i64 = 50;
+
     for line in input.lines() {
-        println!("Line: {}", line);
+        let rotations: String = line.chars().skip(1).collect();
+        let mut rotations: i64 = rotations.parse().unwrap();
+
+        // deal with full cycles
+        if rotations >= 100 {
+            // set rotations to remainder after full cycles are taken into acccount
+            rotations %= 100;
+        }
+
+        // rotate dial
+        if line.starts_with("L") {
+            dial -= rotations;
+        } else {
+            dial += rotations;
+        }
+
+        // account for partial cycles
+        if dial < 0 {
+            dial += 100;
+        } else if dial > 99 {
+            dial -= 100;
+        }
+
+        // increment password
+        if dial == 0 {
+            password += 1;
+        }
     }
+    println!("The password is: {}", password);
 }
 
 fn main() {
