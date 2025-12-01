8fe06edc35d16253152cc830e6f2445706f7178a
Gregory Travis <gtravis6@gatech.edu>
2025-12-01 07:37:25 -0500

    Starting part 1

--- 2025/Live/day01/src/main.rs
+++ 2025/Live/day01/src/main.rs
@@ -1,3 +1,20 @@
+// advent-2025 day 1
+
+fn part1(input: &String) {
+    for line in input.lines() {
+        println!("Line: {}", line);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    for &file in &["test.txt", "input.txt"] {
+        let input = std::fs::read_to_string(format!("day01/{}", file))
+            .expect(&format!("Failed to read file: {}", file))
+            .trim_end()
+            .to_string();
+
+        println!("Part 1: {}:", file);
+        part1(&input);
+        println!("Part 2: {}:", file);
+    }
 }
