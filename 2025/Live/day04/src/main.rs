7099005ab8388205ade94450dd633a682ec9f0a5
Gregory Travis <gtravis6@gatech.edu>
2025-12-04 08:15:07 -0500

    Finished day 4 part 2

--- 2025/Live/day04/src/main.rs
+++ 2025/Live/day04/src/main.rs
@@ -34,6 +34,47 @@ fn part1(input: &String) {
     println!("The forklift can reach {} rolls!", forklift_can_reach);
 }
 
+fn part2(input: &String) {
+   let mut forklift_can_reach: i32 = 0;
+   let mut grid: Vec<Vec<char>> = vec![];
+
+    for line in input.lines() {
+        grid.push(line.chars().collect());
+    }
+
+    let mut removed_rolls: i32 = 1;
+    while removed_rolls > 0 {
+        removed_rolls = 0;
+        for row in 0..grid.len() {
+            for col in 0..grid[row].len() {
+                // skip position if there is no roll here
+                if grid[row][col] != '@' {
+                    continue;
+                }
+                let mut adjacent_rolls: i32 = 0;
+                // iterate around current position and count how many rolls there are
+                for r in cmp::max(row as i32 - 1, 0)..=cmp::min(row as i32 + 1, grid.len() as i32 - 1) {
+                    for c in cmp::max(col as i32 - 1, 0)..=cmp::min(col as i32 + 1, grid[row].len() as i32 - 1) {
+                        if grid[r as usize][c as usize] == '@' {
+                            adjacent_rolls += 1;
+                        }
+                    }
+                }
+                // decrement 1 to avoid counting the roll at current position
+                adjacent_rolls -= 1;
+                // if less than four adjacent rolls then the forklift can reach it!
+                if adjacent_rolls < 4 {
+                    forklift_can_reach += 1;
+                    removed_rolls += 1;
+                    grid[row][col] = ' ';
+                }
+            }
+        }
+    }
+
+    println!("The forklift can reach {} rolls!", forklift_can_reach);
+}
+
 fn main() {
     for &file in &["test.txt", "input.txt"] {
         let input = std::fs::read_to_string(format!("day04/{}", file))
@@ -43,5 +84,8 @@ fn main() {
 
         println!("Part 1: {}:", file);
         part1(&input);
+
+        println!("Part 2: {}:", file);
+        part2(&input);
     }
 }
