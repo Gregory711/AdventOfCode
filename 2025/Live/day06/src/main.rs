8397f0582de41295093cda1d645a26228f93dc02
Gregory Travis <gtravis6@gatech.edu>
2025-12-06 11:13:09 -0500

    Almost got test data correct for part 2 on first compile

--- 2025/Live/day06/src/main.rs
+++ 2025/Live/day06/src/main.rs
@@ -27,6 +27,54 @@ fn part1(input: &String) {
     println!("The grand total is {}", total);
 }
 
+fn part2(input: &String) {
+    let mut grid: Vec<Vec<char>> = vec![];
+    let mut ops: Vec<char> = vec![];
+
+    // wrangle input
+    for line in input.lines() {
+        if line.contains('*') || line.contains('+') {
+            ops = line.chars().collect();
+        } else {
+            grid.push(line.chars().collect());
+        }
+    }
+
+    // go backwards over input to build numbers and construct subtotals
+    let mut total: i64 = 0;
+    let mut numbers: Vec<i64> = vec![];
+    for col in (0..ops.len()).rev() {
+        // Build number
+        let mut number_str: String = String::from("");
+        for row in 0..grid.len() {
+            // some numbers don't use all digits
+            if grid[row][col] == ' ' {
+                continue;
+            }
+            number_str.push(grid[row][col]);
+        }
+        // continue if this is empty column in between math problems
+        if number_str.is_empty() {
+            continue;
+        }
+        numbers.push(number_str.parse().unwrap());
+        // If ops column then calculate subtotal and clear numbers vector
+        if ops[col] != ' ' {
+            let mut subtotal: i64 = numbers[0];
+            for i in 1..numbers.len() {
+                if ops[col] == '*' {
+                    subtotal *= numbers[i];
+                } else {
+                    subtotal += numbers[i];
+                }
+            }
+            numbers.clear();
+            total += subtotal;
+        }
+    }
+    println!("The grand total is {}", total);
+}
+
 fn main() {
     for &file in &["test.txt", "input.txt"] {
         let input = std::fs::read_to_string(format!("day06/{}", file))
@@ -36,5 +84,8 @@ fn main() {
 
         println!("Part 1: {}:", file);
         part1(&input);
+
+        println!("Part 2: {}:", file);
+        part2(&input);
     }
 }
