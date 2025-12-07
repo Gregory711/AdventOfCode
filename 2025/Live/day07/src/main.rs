e2e6b442b1a1c8d864cae7274b73b0319af4a09e
Gregory Travis <gtravis6@gatech.edu>
2025-12-07 10:23:27 -0500

    Solved day 7 part 2

--- 2025/Live/day07/src/main.rs
+++ 2025/Live/day07/src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::collections::HashMap;
 
 fn part1(input: &String) {
     let mut split_count: i64 = 0;
@@ -25,6 +26,35 @@ fn part1(input: &String) {
     println!("The number if splits was {} that's bananas!", split_count);
 }
 
+fn part2(input: &String) {
+    // mapping number of beams from any timeline heading down <row index, beam count>
+    let mut beams_from_above: HashMap<usize, usize> = HashMap::new();
+    for line in input.lines() {
+        let mut beams_to_below: HashMap<usize, usize> = HashMap::new();
+        let row: Vec<char> = line.chars().collect();
+        for col in 0..row.len() {
+            if beams_from_above.contains_key(&col) {
+                let add_beams: usize = *beams_from_above.get(&col).unwrap();
+                if row[col] == '.' {
+                    *beams_to_below.entry(col).or_insert(0) += add_beams;
+                } else {
+                    // split
+                    *beams_to_below.entry(col - 1).or_insert(0) += add_beams;
+                    *beams_to_below.entry(col + 1).or_insert(0) += add_beams;
+                }
+            } else if row[col] == 'S' {
+                beams_to_below.insert(col, 1);
+            }
+        }
+        beams_from_above = beams_to_below;
+    }
+    let mut count: usize = 0;
+    for beams in beams_from_above.values() {
+        count += beams;
+    }
+    println!("There are {} different timelines!", count);
+}
+
 fn main() {
     for &file in &["test.txt", "input.txt"] {
         let input = std::fs::read_to_string(format!("day07/{}", file))
@@ -34,5 +64,8 @@ fn main() {
 
         println!("Part 1: {}:", file);
         part1(&input);
+
+        println!("Part 2: {}:", file);
+        part2(&input);
     }
 }
