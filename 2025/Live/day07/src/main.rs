6631185e94e1920127949795a6acfe43daa6de85
Gregory Travis <gtravis6@gatech.edu>
2025-12-07 09:50:12 -0500

    Solved day 7 part 1

--- 2025/Live/day07/src/main.rs
+++ 2025/Live/day07/src/main.rs
@@ -1,3 +1,38 @@
+use std::collections::HashSet;
+
+fn part1(input: &String) {
+    let mut split_count: i64 = 0;
+    let mut beams_from_above: HashSet<usize> = HashSet::new();
+    for line in input.lines() {
+        let mut beams_to_below: HashSet<usize> = HashSet::new();
+        let row: Vec<char> = line.chars().collect();
+        for col in 0..row.len() {
+            if beams_from_above.contains(&col) {
+                if row[col] == '.' {
+                    beams_to_below.insert(col);
+                } else {
+                    // split
+                    beams_to_below.insert(col - 1);
+                    beams_to_below.insert(col + 1);
+                    split_count += 1;
+                }
+            } else if row[col] == 'S' {
+                beams_to_below.insert(col);
+            }
+        }
+        beams_from_above = beams_to_below;
+    }
+    println!("The number if splits was {} that's bananas!", split_count);
+}
+
 fn main() {
-    println!("Hello, world!");
+    for &file in &["test.txt", "input.txt"] {
+        let input = std::fs::read_to_string(format!("day07/{}", file))
+            .expect(&format!("Failed to read file: {}", file))
+            .trim_end()
+            .to_string();
+
+        println!("Part 1: {}:", file);
+        part1(&input);
+    }
 }
