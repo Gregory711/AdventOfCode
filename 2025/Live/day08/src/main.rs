6f6ff0f5731a6bbfb43714356d72927668b15154
Gregory Travis <gtravis6@gatech.edu>
2025-12-08 20:52:25 -0500

    Read in input

--- 2025/Live/day08/src/main.rs
+++ 2025/Live/day08/src/main.rs
@@ -10,6 +10,20 @@ impl Point {
     }
 }
 
+fn part1(input: &String) {
+    let mut points: Vec<Point> = vec!();
+    for line in input.lines() {
+        let coords: Vec<&str> = line.split(',').collect();
+        points.push(
+            Point{
+                x: coords[0].parse().unwrap(),
+                y: coords[1].parse().unwrap(),
+                z: coords[2].parse().unwrap()
+            }
+        );
+    }
+}
+
 fn main() {
     let a = Point{x: 0, y: 0, z: 0};
     let b = Point{x: 1, y: 2, z: 3};
@@ -19,5 +33,16 @@ fn main() {
         println!("My distance function works! Pythagoras would be proud :)");
     } else {
         println!("ruh roh, my distance function is broken, expected 14, got: {}", a.distance_to(&b));
+    }
+
+    //for &file in &["test.txt", "input.txt"] {
+    for &file in &["test.txt"] {
+        let input = std::fs::read_to_string(format!("day08/{}", file))
+            .expect(&format!("Failed to read file: {}", file))
+            .trim_end()
+            .to_string();
+
+        println!("Part 1: {}:", file);
+        part1(&input);
     }
 }
