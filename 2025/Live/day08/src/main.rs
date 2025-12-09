623507feff4fd4f6b43a9a70cd090ac86fb4892c
Gregory Travis <gtravis6@gatech.edu>
2025-12-08 21:12:39 -0500

    Refactored to add required parameter to runs

--- 2025/Live/day08/src/main.rs
+++ 2025/Live/day08/src/main.rs
@@ -10,7 +10,7 @@ impl Point {
     }
 }
 
-fn part1(input: &String) {
+fn part1(input: &String, boxes_to_connect_count: i64) {
     let mut points: Vec<Point> = vec!();
     for line in input.lines() {
         let coords: Vec<&str> = line.split(',').collect();
@@ -35,14 +35,15 @@ fn main() {
         println!("ruh roh, my distance function is broken, expected 14, got: {}", a.distance_to(&b));
     }
 
-    //for &file in &["test.txt", "input.txt"] {
-    for &file in &["test.txt"] {
+    //let runs = vec![(&"test.txt", 10 as i64), (&"input.txt", 100 as i64)]; // literal vector of tuples :)
+    let runs = vec![(&"test.txt", 10 as i64)];
+    for (file, boxes_to_connect_count) in &runs {
         let input = std::fs::read_to_string(format!("day08/{}", file))
             .expect(&format!("Failed to read file: {}", file))
             .trim_end()
             .to_string();
 
-        println!("Part 1: {}:", file);
-        part1(&input);
+        println!("Part 1: {}, {}:", file, boxes_to_connect_count);
+        part1(&input, *boxes_to_connect_count);
     }
 }
