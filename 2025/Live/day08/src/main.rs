6f13a73f2952f02c73f4daa63636e05b1b42342a
Gregory Travis <gtravis6@gatech.edu>
2025-12-08 20:31:21 -0500

    Added and tested a distance method for my Point struct

--- 2025/Live/day08/src/main.rs
+++ 2025/Live/day08/src/main.rs
@@ -4,5 +4,20 @@ struct Point {
   z: i64
 }
 
+impl Point {
+    fn distance_to(&self, other: &Point) -> i64 {
+        return (other.x - self.x).pow(2) + (other.y - self.y).pow(2) + (other.z - self.z).pow(2);
+    }
+}
+
 fn main() {
+    let a = Point{x: 0, y: 0, z: 0};
+    let b = Point{x: 1, y: 2, z: 3};
+    // distance should be diff of each dimension squared combined which for this example
+    // is 1*1 + 2*2 + 3*3 = 1 + 4 + 9 + 14
+    if a.distance_to(&b) == 14 {
+        println!("My distance function works! Pythagoras would be proud :)");
+    } else {
+        println!("ruh roh, my distance function is broken, expected 14, got: {}", a.distance_to(&b));
+    }
 }
