e2bbd9272baf793ef93144a3d90b5cf0ff9ff2ba
Gregory Travis <gtravis6@gatech.edu>
2025-12-10 21:59:23 -0500

    Trashed broken overcomplicated algo and replaced it with simple elegant working one to finish solving day 8 part 1

--- 2025/Live/day08/src/main.rs
+++ 2025/Live/day08/src/main.rs
@@ -66,56 +66,27 @@ fn part1(input: &String, boxes_to_connect_count: usize) {
 
     connections.sort();
 
-    // create circuits using the cheapest boxes_to_connect_count number of connections
+    // Make the boxes_to_connect_count cheapest connections and form circuits using those
+    // Algorithm to do this is as follows:
+    // Iterate over the boxes_to_connect_count cheapest connections and for each one:
+    //   Combine that new connection into a new circuit with all other existing circuits that
+    //   overlap with it at all i.e. contain any of the same points, like legos snapping together
+    //   Then add this new mega circuit to list with remaining circuits and go to next iteration
     let mut circuits: Vec<HashSet<Point>> = vec!();
-    let mut added_connections_count: usize = 0;
-    let mut i: usize = 0;
-    while added_connections_count < boxes_to_connect_count {
-        // add ith connection to existing circuit if possible i.e. if one point in connection
-        // already in the circuit
-        let mut added_to_circuit: bool = false;
-        let mut combine_circuits: Vec<usize> = vec!();
-        for (index, circuit) in circuits.iter_mut().enumerate() {
-            if circuit.contains(&connections[i].a) && circuit.contains(&connections[i].b) {
-                // case where both connections already in circuits
-                added_to_circuit = true;
-                combine_circuits.push(index);
-            } else if added_to_circuit && (circuit.contains(&connections[i].a) || circuit.contains(&connections[i].b)) {
-                // case where we already connected one of these points to a circuit so we can now
-                // combine these circuits into one big circuit!
-                // We'll do this by adding everything from this circuit to the prior circuit and
-                // clear this circuit
-                combine_circuits.push(index);
-            } else if circuit.contains(&connections[i].a) || circuit.contains(&connections[i].b) {
-                // case where we are just adding this connection to an existing circuit because it
-                // shares a point
-                if circuit.contains(&connections[i].a) {
-                    //println!("Adding {} to a circuit", connections[i].b);
-                    circuit.insert(connections[i].b.clone());
-                } else {
-                    //println!("Adding {} to a circuit", connections[i].a);
-                    circuit.insert(connections[i].a.clone());
-                }
-                added_to_circuit = true;
-                combine_circuits.push(index);
+    for i in 0..boxes_to_connect_count {
+        let mut new_circuit = HashSet::from([connections[i].a.clone(), connections[i].b.clone()]);
+        let mut new_circuits: Vec<HashSet<Point>> = vec!();
+        for circuit in circuits {
+            // add circuit to new_circuit if there is overlap else add it wholesale to new list
+            if !new_circuit.is_disjoint(&circuit) {
+                // add everything from circuit to new_circuit!
+                new_circuit.extend(circuit);
+            } else {
+                new_circuits.push(circuit);
             }
         }
-        if !added_to_circuit {
-            //println!("Creating a new circuit using {} and {}", connections[i].a, connections[i].b);
-            circuits.push(HashSet::from([connections[i].a.clone(), connections[i].b.clone()]));
-        }
-        // combine circuits!
-        for index in 1..combine_circuits.len() {
-            let circuits_clone = circuits[index].clone();
-            for point in circuits_clone.iter().enumerate() {
-                circuits[combine_circuits[0]].insert(point.1.clone());
-            }
-            circuits[index].clear();
-        }
-        i += 1;
-        added_connections_count += 1;
-        println!("After the {}th loop:", i);
-        print_circuits(&circuits);
+        new_circuits.push(new_circuit);
+        circuits = new_circuits;
     }
 
     // calculate the size of the circuits
@@ -129,11 +100,11 @@ fn part1(input: &String, boxes_to_connect_count: usize) {
     circuits_sizes.reverse();
     
     let product = circuits_sizes[0] * circuits_sizes[1] * circuits_sizes[2];
-    println!("There are {} circuits total", circuits.len());
+    /*println!("There are {} circuits total", circuits.len());
     println!("From biggest to smallest the circuits have sizes:");
     for circuit_size in circuits_sizes {
         println!("{}", circuit_size);
-    }
+    }*/
     println!("The result of multiplying the sizes of the three biggest circuits is: {}", product);
 }
 
@@ -148,8 +119,8 @@ fn main() {
         println!("ruh roh, my distance function is broken, expected 14, got: {}", a.distance_to(&b));
     }
 
-    let runs = vec![(&"test.txt", 10 as usize)];
-    //let runs = vec![(&"test.txt", 10 as usize), (&"input.txt", 1000 as usize)]; // literal vector of tuples :)
+    //let runs = vec![(&"test.txt", 10 as usize)];
+    let runs = vec![(&"test.txt", 10 as usize), (&"input.txt", 1000 as usize)]; // literal vector of tuples :)
         for (file, boxes_to_connect_count) in &runs {
         let input = std::fs::read_to_string(format!("day08/{}", file))
             .expect(&format!("Failed to read file: {}", file))
