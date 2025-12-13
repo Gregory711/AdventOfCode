328ce286c4581d4f502ace0a54a4a119a70adf49
Gregory Travis <gtravis6@gatech.edu>
2025-12-13 13:57:14 -0500

    Starting day 9 part 2

--- 2025/Live/day09/src/main.rs
+++ 2025/Live/day09/src/main.rs
@@ -1,8 +1,14 @@
 use std::cmp;
+use std::collections::HashMap;
 
 struct Point {
     x: i64,
     y: i64
+}
+
+struct Edge {
+    start: i64,
+    end: i64
 }
 
 fn rect_area(corner_a: &Point, corner_b: &Point) -> i64 {
@@ -27,6 +33,45 @@ fn part1(input: &String) {
     println!("The max area is: {}", max_area);
 }
 
+fn part2(input: &String) {
+    let mut points: Vec<Point> = vec!();
+
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split(",").collect();
+        points.push(Point{ x: parts[0].parse().unwrap(), y: parts[1].parse().unwrap()});
+    }
+
+    // row_edges are on a particular row (y) and start/stop on particular columns
+    // likewise col_edges are on a particular column (x) and start/stop on particular rows
+    let mut row_edges: HashMap<i64, Edge> = HashMap::new();
+    let mut col_edges: HashMap<i64, Edge> = HashMap::new();
+    for point in points {
+        // upsert row edge if needed
+        if !row_edges.contains_key(&point.y) {
+            row_edges.insert(point.y, Edge{ start: point.x, end: point.x });
+        } else {
+            let row_edge: &Edge = row_edges.get(&point.y).unwrap();
+            let updated_edge: Edge = Edge{ start: cmp::min(row_edge.start, point.y), end: cmp::max(row_edge.end, point.x) };
+            if row_edge.start != updated_edge.start || row_edge.end != updated_edge.end {
+                row_edges.remove(&point.y);
+                row_edges.insert(point.y, updated_edge);
+            }
+        }
+
+        // upsert col edge if needed
+        if !col_edges.contains_key(&point.x) {
+            col_edges.insert(point.x, Edge{ start: point.y, end: point.y });
+        } else {
+            let col_edge: &Edge = col_edges.get(&point.x).unwrap();
+            let updated_edge: Edge = Edge{ start: cmp::min(col_edge.start, point.x), end: cmp::max(col_edge.end, point.y) };
+            if col_edge.start != updated_edge.start || col_edge.end != updated_edge.end {
+                col_edges.remove(&point.x);
+                col_edges.insert(point.x, updated_edge);
+            }
+        }
+    }
+}
+
 fn main() {
     let a = Point{ x: 2, y: 2 };
     let b = Point{ x: 3, y: 3 };
@@ -37,8 +82,8 @@ fn main() {
         println!("ruh roh");
     }
 
-    for &file in &["test.txt", "input.txt"] {
-    //for &file in &["test.txt"] {
+    //for &file in &["test.txt", "input.txt"] {
+    for &file in &["test.txt"] {
         let input = std::fs::read_to_string(format!("day09/{}", file))
             .expect(&format!("Failed to read file: {}", file))
             .trim_end()
@@ -46,5 +91,8 @@ fn main() {
 
         println!("Part 1: {}:", file);
         part1(&input);
+
+        println!("Part 2: {}:", file);
+        part2(&input);
     }
 }
