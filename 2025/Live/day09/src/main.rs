6efa5551d855ef11738be3b3e4798acf54c6862c
Gregory Travis <gtravis6@gatech.edu>
2025-12-13 10:05:50 -0500

    Fixed off by one error and corrected tests and finished part 1

--- 2025/Live/day09/src/main.rs
+++ 2025/Live/day09/src/main.rs
@@ -1,10 +1,12 @@
+use std::cmp;
+
 struct Point {
     x: i64,
     y: i64
 }
 
 fn rect_area(corner_a: &Point, corner_b: &Point) -> i64 {
-    return i64::abs(corner_a.x - corner_b.x) * i64::abs(corner_a.y - corner_b.y);
+    return (i64::abs(corner_a.x - corner_b.x) + 1) * (i64::abs(corner_a.y - corner_b.y) + 1);
 }
 
 fn part1(input: &String) {
@@ -14,20 +16,29 @@ fn part1(input: &String) {
         let parts: Vec<&str> = line.split(",").collect();
         points.push(Point{ x: parts[0].parse().unwrap(), y: parts[1].parse().unwrap()});
     }
+
+    let mut max_area: i64 = 0;
+    for i in 0..points.len() {
+        for j in (i + 1)..points.len() {
+            max_area = cmp::max(max_area, rect_area(&points[i], &points[j]));
+        }
+    }
+
+    println!("The max area is: {}", max_area);
 }
 
 fn main() {
     let a = Point{ x: 2, y: 2 };
     let b = Point{ x: 3, y: 3 };
     let c = Point{ x: 0, y: 0 };
-    if rect_area(&a, &b) == 1 && rect_area(&a, &c) == 4 {
+    if rect_area(&a, &b) == 4 && rect_area(&a, &c) == 9 {
         println!("rect_area is working as intended!");
     } else {
         println!("ruh roh");
     }
 
-    //for &file in &["test.txt", "input.txt"] {
-    for &file in &["test.txt"] {
+    for &file in &["test.txt", "input.txt"] {
+    //for &file in &["test.txt"] {
         let input = std::fs::read_to_string(format!("day09/{}", file))
             .expect(&format!("Failed to read file: {}", file))
             .trim_end()
