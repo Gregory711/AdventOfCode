183824aba7754d76e23a50dc47eac130ca5c0388
Gregory Travis <gtravis6@gatech.edu>
2025-12-13 09:56:42 -0500

    Read in input and add test data

--- 2025/Live/day09/src/main.rs
+++ 2025/Live/day09/src/main.rs
@@ -7,6 +7,15 @@ fn rect_area(corner_a: &Point, corner_b: &Point) -> i64 {
     return i64::abs(corner_a.x - corner_b.x) * i64::abs(corner_a.y - corner_b.y);
 }
 
+fn part1(input: &String) {
+    let mut points: Vec<Point> = vec!();
+
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split(",").collect();
+        points.push(Point{ x: parts[0].parse().unwrap(), y: parts[1].parse().unwrap()});
+    }
+}
+
 fn main() {
     let a = Point{ x: 2, y: 2 };
     let b = Point{ x: 3, y: 3 };
@@ -15,5 +24,16 @@ fn main() {
         println!("rect_area is working as intended!");
     } else {
         println!("ruh roh");
+    }
+
+    //for &file in &["test.txt", "input.txt"] {
+    for &file in &["test.txt"] {
+        let input = std::fs::read_to_string(format!("day09/{}", file))
+            .expect(&format!("Failed to read file: {}", file))
+            .trim_end()
+            .to_string();
+
+        println!("Part 1: {}:", file);
+        part1(&input);
     }
 }
