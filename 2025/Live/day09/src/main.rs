292e62f8bd8701a70e983ec684680e3f49b15392
Gregory Travis <gtravis6@gatech.edu>
2025-12-28 16:36:02 -0500

    Setting up skaffolding for new day 9 part 1 solution

--- 2025/Live/day09/src/main.rs
+++ 2025/Live/day09/src/main.rs
@@ -1,3 +1,28 @@
+fn part1(input: &String) {
+    let mut points: Vec<(i64, i64)> = vec!();
+
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split(",").collect();
+        points.push((parts[0].parse().unwrap(), parts[1].parse().unwrap()));
+    }
+
+    let mut max_area: i64 = 0;
+    for i in 0..points.len() {
+        for j in (i + 1)..points.len() {
+            //let rect = polygon
+        }
+    }
+}
+
 fn main() {
-    println!("test");
+    //for &file in &["test.txt", "input.txt"] { 
+    for &file in &["test.txt"] {
+        let input = std::fs::read_to_string(format!("day09/{}", file))
+            .expect(&format!("Failed to read file: {}", file))
+            .trim_end()
+            .to_string();
+
+        println!("Part 1: {}:", file);
+        part1(&input);
+    }
 }
